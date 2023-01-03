7d7784b10076f14729d96e4302f7b5723341a92d
shakunage <veikka.junttila1@gmail.com>
2023-01-03 15:57:56 +0900

    resume page fix

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Box, Button } from '@mui/material/';
-import { Document, Page, pdfjs } from 'react-pdf';
 import resumefile from "../assets/pdfs/resume.pdf"
-
-pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.min.js';
 
 const Resume = () => {
 
@@ -17,9 +14,9 @@ const Resume = () => {
         width: "60%"}}>
         <h1>resume</h1>
         <Button style={{justifyContent: "flex-start"}}><a style={{color: "inherit", textDecoration: "inherit"}} href={resumefile} download>download resume</a></Button>
-        <Document file={resumefile}>
-        <Page height={650} pageNumber={1} />
-        </Document>
+        <div>
+        <embed style={{height: "100vh", width: "100%"}} src={resumefile} type="application/pdf"/>
+        </div>
       </Box>
     </>
     
